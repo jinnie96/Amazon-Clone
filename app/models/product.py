2429c301c39b862d1fd7a051c9d026615c1fdda2
Karandeep <jinnie1996@hotmail.com>
2022-06-15 00:32:14 -0700

    reviews seeding

--- app/models/product.py
+++ app/models/product.py
@@ -4,10 +4,9 @@ class Product(db.Model):
     __tablename__ = 'product'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(500), nullable=False)
-    description = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
     price = db.Column(db.Numeric(4,2), nullable=False)
-
 
     def to_dict(self):
         return {
---
+++ app/seeds/reviews.py
@@ -0,0 +1,25 @@
+from app.models import db, Review
+
+def seed_reviews():
+    review1 = Review(
+        reviewer_id=2, product_id=2, description='Not working!', title='Do not buy!!!', rating=1)
+    review2 = Review(
+        reviewer_id=1, product_id=1, description='Great product. I have had no problems.', title='Awesome', rating=5)
+    review3 = Review(
+        reviewer_id=3, product_id=3, description='Fun, but batteries run out too fast.', title='Solid', rating=4)
+
+    db.session.add(review1)
+    db.session.add(review2)
+    db.session.add(review3)
+
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE the users table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and RESET IDENTITY
+# resets the auto incrementing primary key, CASCADE deletes any
+# dependent entities
+def undo_users():
+    db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
+    db.session.commit()
