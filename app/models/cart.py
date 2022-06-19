48371679e07ded5fa93a29d034e938ae69f995a4
Karandeep <jinnie1996@hotmail.com>
2022-06-18 17:43:45 -0700

    error fix

--- app/models/cart.py
+++ app/models/cart.py
@@ -5,16 +5,16 @@ class Cart(db.Model):
     __tablename__ = 'cart'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 
-    users = relationship("User", foreign_keys=[user_id], back_populates="cart")
+    # users = relationship("User", foreign_keys=[user_id], back_populates="cart")
     products = relationship("Product", foreign_keys=[product_id], back_populates="cart")
     product_cart = relationship("product_cart", foreign_keys="Product_Cart.cart_id", back_populates='cart')
 
     def to_dict(self):
         return {
             'id': self.id,
-            'user_id': self.user_id,
+            # 'user_id': self.user_id,
             'product_id': self.product_id,
         }
