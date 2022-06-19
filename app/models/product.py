33ce7a1b6a68d3bb5a28166a4613cc40aa5d98ca
Karandeep <jinnie1996@hotmail.com>
2022-06-18 20:12:51 -0700

    no more errors

--- app/models/product.py
+++ app/models/product.py
@@ -2,16 +2,16 @@ from .db import db
 from sqlalchemy.orm import relationship
 
 class Product(db.Model):
-    __tablename__ = 'product'
+    __tablename__ = 'products'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(500), nullable=False)
     price = db.Column(db.Numeric(4,2), nullable=False)
 
-    review = relationship("Review", foreign_keys="Review.product_id", back_populates='product')
-    cart = relationship("Cart", foreign_keys="Cart.product_id", back_populates='product')
-    product_cart = relationship("Product_Cart", foreign_keys="Product_Cart.product_id", back_populates='product')
+    reviews = relationship("Review", foreign_keys="Review.product_id", back_populates='products')
+    cart = relationship("Cart", foreign_keys="Cart.product_id", back_populates='products')
+    product_cart = relationship("Product_Cart", foreign_keys="Product_Cart.product_id", back_populates='products')
 
 
     def to_dict(self):
--- app/models/review.py
+++ app/models/review.py
@@ -3,17 +3,17 @@ from sqlalchemy.orm import relationship
 
 
 class Review(db.Model):
-    __tablename__ = 'review'
+    __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
     reviewer_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
     description = db.Column(db.String(200), nullable=False)
     title = db.Column(db.String(50), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
 
-    users = relationship("User", foreign_keys=[reviewer_id], back_populates="review")
-    product = relationship("Product", foreign_keys=[product_id], back_populates="review")
+    users = relationship("User", foreign_keys=[reviewer_id], back_populates="reviews")
+    products = relationship("Product", foreign_keys=[product_id], back_populates="reviews")
 
 
     def to_dict(self):
