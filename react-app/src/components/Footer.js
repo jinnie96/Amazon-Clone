03406596a0214e8215822de5cd004d90bd698b13
Karandeep <jinnie1996@hotmail.com>
2022-07-03 00:54:25 -0700

    working on footer

---
+++ react-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+const Footer = () => {
+
+    return (
+        <h1>hi</h1>
+    )
+}
+
+export default Footer
