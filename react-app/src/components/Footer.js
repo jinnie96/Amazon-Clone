d0d680760172e9d12ebab86f365e0e94005a2331
Karandeep <jinnie1996@hotmail.com>
2022-07-03 22:09:02 -0700

    back to top div created

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import './Footer.css'
 
 const Footer = () => {
 
     return (
-        <h1>hi</h1>
+        <div>
+            <div className="top">Back to top</div>
+            <div className="bottom">
+               <div className="get">Get to Know Us</div>
+            </div>
+        </div>
+
     )
 }
 
