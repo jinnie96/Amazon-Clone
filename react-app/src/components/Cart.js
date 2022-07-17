b832b39b7cc22e3d30624de0ce5636029358621c
Karandeep <jinnie1996@hotmail.com>
2022-07-17 01:44:51 -0700

    working on delete product function

--- react-app/src/components/Cart.js
+++ react-app/src/components/Cart.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import './Cart.css'
+import { deleteCarts } from '../store/carts';
 
 const Cart = () => {
     const cart = useSelector(state => state.cart);
     console.log(cart, "@@@")
+    const dispatch = useDispatch()
+
+
+    const deleteProduct = (e) => {
+        console.log(e.target.parentNode.firstChild.id)
+        const id = e.target.parentNode.firstChild.id
+        dispatch(deleteCarts(id))
+    }
+
     return (
         <div>
             {/* <h1>cart</h1> */}
@@ -13,7 +24,10 @@ const Cart = () => {
                 Object.keys(cart).map((oneKey,i)=>{
                     {console.log("KEYYYY", cart[oneKey])}
                     return (
-                        <li key={i}>{cart[oneKey].name}</li>
+                        <div className='productBox'>
+                            <li key={i} id={i}>{cart[oneKey].name}</li>
+                            <p id='deleteBtn' onClick={deleteProduct}>Delete</p>
+                        </div>
                     )
                 })
                 }
