9b838358b5f1f878b34c38bfee4d1a34a99c8975
Karandeep <jinnie1996@hotmail.com>
2022-07-18 16:01:16 -0700

    delelete and get review apis done

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from app.models import Product
+from app.models import Review, db
 from flask_login import login_required
 
 # import simplejson as json
@@ -8,3 +8,21 @@ review_routes = Blueprint('reviews', __name__)
 
 @review_routes.route('/<int:id>')
 @login_required
+def getReviews(id):
+    reviews = Review.query.filter(Review.product_id == id).all()
+    return {
+            "reviews": [review.to_dict() for review in reviews]
+        }
+
+@review_routes.route('/<int:id>')
+@login_required
+def deleteReview(id):
+    review = Review.query.get(id)
+    db.session.delete(review)
+    db.session.commit()
+    return "Comment deleted"
+
+@review_routes.route('/<int:id>')
+@login_required
+def editReview(id):
+    review = Review.query.get(id)
