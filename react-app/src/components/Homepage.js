653097548cbbe3e387d8a50e2054d400e1430bfe
Karandeep <jinnie1996@hotmail.com>
2022-07-30 23:36:36 -0700

    home page styling

--- react-app/src/components/Homepage.js
+++ react-app/src/components/Homepage.js
@@ -60,10 +60,10 @@ function Homepage() {
                     Fantasy
                 </div>
                 <div class="sportsPics">
-                    <NavLink to={'fantasy/1'}><img id="gameOfThrones" src="https://images-na.ssl-images-amazon.com/images/I/81q1AybR-ZL.jpg"></img></NavLink>
-                    <NavLink to={'fantasy/2'}><img id="nameOfWind" src="https://images-na.ssl-images-amazon.com/images/I/91b8oNwaV1L.jpg"></img></NavLink>
-                    <NavLink to={'fantasy/3'}><img id="lordOfTheRings" src="https://images-na.ssl-images-amazon.com/images/I/71jLBXtWJWL.jpg"></img></NavLink>
-                    <NavLink to={'fantasy/4'}><img id="colorOfMagic" src="https://images-na.ssl-images-amazon.com/images/I/91u24IHWS8L.jpg"></img></NavLink>
+                    <NavLink to={'sports/1'}><img id="gameOfThrones" src="https://images-na.ssl-images-amazon.com/images/I/81q1AybR-ZL.jpg"></img></NavLink>
+                    <NavLink to={'sports/2'}><img id="nameOfWind" src="https://images-na.ssl-images-amazon.com/images/I/91b8oNwaV1L.jpg"></img></NavLink>
+                    <NavLink to={'sports/3'}><img id="lordOfTheRings" src="https://images-na.ssl-images-amazon.com/images/I/71jLBXtWJWL.jpg"></img></NavLink>
+                    <NavLink to={'sports/4'}><img id="colorOfMagic" src="https://images-na.ssl-images-amazon.com/images/I/91u24IHWS8L.jpg"></img></NavLink>
                 </div>
             </div>
             <div className="books">
