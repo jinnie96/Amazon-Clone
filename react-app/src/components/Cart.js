751dde909fdf75987fe6e83bd600948a423b2cac
Karandeep <jinnie1996@hotmail.com>
2022-08-08 00:03:32 -0700

    fixed quantity bug in cart

--- react-app/src/components/Cart.js
+++ react-app/src/components/Cart.js
@@ -11,25 +11,29 @@ const Cart = () => {
     const dispatch = useDispatch()
     let total = 0
     let subTotal = 0
+    let count = 1
     useEffect(() => {
-        (async() => {
-        })();
-    }, [dispatch]);
+        if (cart) {
+            // subTotal = Object.keys(cart).length
+            console.log("inside")
+            Object.keys(cart).map((oneKey,i)=>{
+                console.log("ONEKEY", cart[oneKey].quantity)
+                subTotal += (cart[oneKey].quantity)
+                total += parseFloat(cart[oneKey].price * cart[oneKey].quantity)
+            })
+        }
+    });
 
-    if (cart) {
-        // subTotal = Object.keys(cart).length
-        Object.keys(cart).map((oneKey,i)=>{
-            console.log("ONEKEY", cart[oneKey].quantity)
-            subTotal += (cart[oneKey].quantity)
-            total += parseFloat(cart[oneKey].price * cart[oneKey].quantity)
-        })
-    }
+    // useEffect(() => {
+    //     dispatch(getCarts(user))
+    // }, [count])
     console.log(total)
 
-    const changeQuantity = (e) => {
+    const changeQuantity = async(e) => {
+        count++
         console.log(e.target.value, e.target.className)
-        dispatch(editCartQuantity(e.target.value, e.target.className))
-        dispatch(getCarts(user))
+        await dispatch(editCartQuantity(e.target.value, e.target.className))
+        await dispatch(getCarts(user))
         // window.location.href = `/`
         // window.location.href = '/cart'
     }
@@ -55,39 +59,45 @@ const Cart = () => {
             <div>
                 {
                 Object.keys(cart).map((oneKey,i)=>{
-                    {console.log("KEYYYY", cart[oneKey])}
-                    return (
-                        <div className='productBox'>
-                            <img id='photoUrl'src={cart[oneKey].photourl}></img>
-                            <div className='cartProductInfo'>
-                                <p key={i} id='productName'>{cart[oneKey].name}</p>
-                                <p id='authorName'>by {cart[oneKey].name}</p>
-                                <p id='instock'>In Stock</p>
-                                <p id='paperback'>Paperback</p>
-                                <p id='returns'>FREE RETURNS</p>
-                                <p id={cart[oneKey].id} className='deleteProdCart' onClick={deleteProduct}>Delete</p>
-                                <select class={cart[oneKey].id} onChange={changeQuantity}>
-                                    <option value='1'>Qty: 1</option>
-                                    <option value='2'>Qty: 2</option>
-                                    <option value='3'>Qty: 3</option>
-                                    <option value='4'>Qty: 4</option>
-                                    <option value='5'>Qty: 5</option>
-                                    <option value='6'>Qty: 6</option>
-                                    <option value='7'>Qty: 7</option>
-                                    <option value='8'>Qty: 8</option>
-                                    <option value='9'>Qty: 9</option>
-                                </select>
+                    {console.log("KEYYYY", parseInt(oneKey), oneKey, cart)}
+                    // {oneKey && ()}
+                    {if(parseInt(oneKey)) {
+                        return (
+                            <div>
+                                <div className='productBox'>
+                                <img id='photoUrl'src={cart[oneKey].photourl}></img>
+                                <div className='cartProductInfo'>
+                                    <p key={i} id='productName'>{cart[oneKey].name}</p>
+                                    <p id='authorName'>by {cart[oneKey].name}</p>
+                                    <p id='instock'>In Stock</p>
+                                    <p id='paperback'>Paperback</p>
+                                    <p id='returns'>FREE RETURNS</p>
+                                    <p id={cart[oneKey].id} className='deleteProdCart' onClick={deleteProduct}>Delete</p>
+                                    <select class={cart[oneKey].id} onChange={changeQuantity}>
+                                        <option value='1'>Qty: 1</option>
+                                        <option value='2'>Qty: 2</option>
+                                        <option value='3'>Qty: 3</option>
+                                        <option value='4'>Qty: 4</option>
+                                        <option value='5'>Qty: 5</option>
+                                        <option value='6'>Qty: 6</option>
+                                        <option value='7'>Qty: 7</option>
+                                        <option value='8'>Qty: 8</option>
+                                        <option value='9'>Qty: 9</option>
+                                    </select>
+                                </div>
+                                <div className='priceDiv'>
+                                    <p id='productPrice'>${cart[oneKey].price}</p>
+                                </div>
                             </div>
-                            <div className='priceDiv'>
-                                <p id='productPrice'>${cart[oneKey].price}</p>
                             </div>
-                        </div>
-                    )
+                        )
+                    }}
+
                 })
                 }
             <div className='subTotalDiv'>
-                <h3 id='subTotalCost'>Subtotal ({subTotal} items): </h3>
-                <h3 id='totalCost'>${total}</h3>
+                <h3 id='subTotalCost'>Subtotal ({cart.count} items): </h3>
+                <h3 id='totalCost'>${cart.total}</h3>
             </div>
             </div>
             <div className='submitOrder'>
