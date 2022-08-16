d66d4bd01cb5bbbba9307e7012ba3abc7eaa1da7
Karandeep <jinnie1996@hotmail.com>
2022-08-15 23:20:24 -0700

    order confirmation page and styling

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,6 +14,7 @@ import Cart from './components/Cart'
 import NewReview from './components/NewReview';
 import EditReview from './components/EditReview'
 import { authenticate } from './store/session';
+import OrderConfirmation from './components/OrderConfirmation';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -61,6 +62,10 @@ function App() {
         <ProtectedRoute path='/sports/edit-review/:id' exact={true} >
           <EditReview/>
         </ProtectedRoute>
+        <ProtectedRoute path='/confirmation' exact={true} >
+          <OrderConfirmation/>
+        </ProtectedRoute>
+
       </Switch>
       <Footer />
     </BrowserRouter>
