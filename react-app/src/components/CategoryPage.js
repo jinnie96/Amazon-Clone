69bfdd81bdebea15b54ef903ea982ac0de673f95
Karandeep <jinnie1996@hotmail.com>
2022-08-28 01:03:32 -0700

    added links to category pages

--- react-app/src/components/CategoryPage.js
+++ react-app/src/components/CategoryPage.js
@@ -8,7 +8,7 @@ import { getSingleProduct } from '../store/products';
 import { addtoCart, getCarts } from '../store/carts';
 import { getAllProducts } from '../store/products';
 import { getAllReviews, deleteOneReview } from '../store/reviews';
-
+import SideBar from './SideBar';
 const CategoryPage = () => {
     const dispatch = useDispatch()
     useEffect(() => {
@@ -24,49 +24,41 @@ const CategoryPage = () => {
     console.log(products)
     console.log((window.location.pathname).slice(10))
     return (
-        <div>
-            <h1 id='genreType'>{window.location.pathname.slice(10)}</h1>
-            <div>
-                <h2 id='bestSellers'>Best Sellers</h2>
+        <div className='categoryPage'>
+            <div className='sideBarDiv'>
+                <SideBar />
             </div>
-            <div className='categoryBooks'>
-            {/* {
-            Object.keys(products).map((oneKey,i)=>{
-                console.log("ONEKEY", products[oneKey].category.includes(window.location.pathname.slice(10).toLowerCase()))
-                // {
-                    products[oneKey].category.includes(window.location.pathname.slice(10).toLowerCase() && (
-                        <h3>{products[oneKey].category}</h3>
-                    ))
+            <div>
+                <h1 id='genreType'>{window.location.pathname.slice(10)}</h1>
+                <div>
+                    <h2 id='bestSellers'>Best Sellers</h2>
+                </div>
+                <div className='categoryBooks'>
+                {Object.keys(products).map((oneKey, i) => (
+                    // {products[oneKey].category.includes(window.location.pathname.slice(10).toLowerCase()) && (
+                        // console.log(products[oneKey])
 
-                // }
-                // subTotal += (cart[oneKey].quantity)
-                // total += parseFloat(cart[oneKey].price * cart[oneKey].quantity)
-            })
-            } */}
-            {Object.keys(products).map((oneKey, i) => (
-                // {products[oneKey].category.includes(window.location.pathname.slice(10).toLowerCase()) && (
-                    // console.log(products[oneKey])
+                        <NavLink className='productGenreBox' to={'/books/' + oneKey}>
+                            <div key={i}>
+                                <div className='bookPic'>
+                                    <img id='photoSize' src={products[oneKey].photourl}></img>
+                                </div>
+                                <div className='bookPrice'>
+                                    ${products[oneKey].price}
+                                </div>
+                                <div className='bookTitle'>
+                                    {products[oneKey].name}
+                                </div>
+                                <div className='bookAuth'>
+                                    by {products[oneKey].author}
+                                </div>
+                            </div>
+                        </NavLink>
 
-                    <NavLink className='productGenreBox' to={'/books/' + oneKey}>
-                        <div key={i}>
-                            <div className='bookPic'>
-                                <img id='photoSize' src={products[oneKey].photourl}></img>
-                            </div>
-                            <div className='bookPrice'>
-                                ${products[oneKey].price}
-                            </div>
-                            <div className='bookTitle'>
-                                {products[oneKey].name}
-                            </div>
-                            <div className='bookAuth'>
-                                by {products[oneKey].author}
-                            </div>
-                        </div>
-                    </NavLink>
+                    // )}
+                ))}
 
-                // )}
-            ))}
-
+                </div>
             </div>
         </div>
     )
