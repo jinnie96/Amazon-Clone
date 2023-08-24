e952fc8546152ed4063db6917dd7460d43f55fbd
Karandeep <jinnie1996@hotmail.com>
2023-08-24 01:09:28 -0700

    upgraded added to cart page

--- react-app/src/components/AddedToCart.js
+++ react-app/src/components/AddedToCart.js
@@ -5,16 +5,24 @@ import './AddedToCart.css'
 
 const AddedToCart = () => {
     const cart = useSelector(state => state.cart);
+    console.log(cart[Object.keys(cart)[0]].photourl, "@@@@@@@@@@@@@@@")
 
     return (
         <div className='productAddedDiv'>
+            <div id='cartpic'>
+                <img id='addedCartPic' src={cart[Object.keys(cart)[0]].photourl}></img>
+            </div>
             <div className='addedToCartDiv'>
                 <img id='greenCheck' src='https://www.pngall.com/wp-content/uploads/12/Green-Check-Transparent.png'></img>
                 <h2 id='added'><b>Added To Cart</b></h2>
             </div>
             <div className='subTotal'>
-                <h3><b>Cart Subtotal:&nbsp;${cart.total}</b></h3>
-                <NavLink to='/cart'><button id='goToCart'>Go to Cart</button></NavLink>
+                <div>
+                    <h3><b>Cart Subtotal:&nbsp;${cart.total}</b></h3>
+                </div>
+                <div>
+                    <NavLink to='/cart'><button id='goToCart'>Go to Cart</button></NavLink>
+                </div>
             </div>
         </div>
     )
